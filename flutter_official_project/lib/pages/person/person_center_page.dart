ac46185bd21476d8be7c9db2560f7be7c0f2ff59
ChengHeMing <994483868@qq.com>
2022-11-14 22:22:26 +0800

    1. PersonPage.

--- flutter_official_project/lib/pages/person/person_center_page.dart
+++ flutter_official_project/lib/pages/person/person_center_page.dart
@@ -1,12 +1,10 @@
-// ignore_for_file: prefer_interpolation_to_compose_strings, avoid_unnecessary_containers
+// ignore_for_file: prefer_interpolation_to_compose_strings, avoid_unnecessary_containers, library_private_types_in_public_api, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
-// import 'package:doubanapp/widgets/image/heart_img_widget.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_official_project/constant/cache_key.dart';
 import 'package:flutter_official_project/constant/constant.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-// import 'package:doubanapp/constant/cache_key.dart';
-// import 'package:doubanapp/main.dart';
 
 typedef VoidCallback = void Function();
 
@@ -19,41 +17,59 @@ class PersonCenterPage extends StatelessWidget {
     debugPrint('build PersonCenterPage');
 
     return Scaffold(
+      // 白色背景
       backgroundColor: Colors.white,
+      // 安全区
       body: SafeArea(
           child: Padding(
+        // 顶部距离 10
         padding: const EdgeInsets.only(top: 10.0),
+        // 自定义滚动视图
         child: CustomScrollView(
+          // 物理效果
           physics: const BouncingScrollPhysics(),
+          // 是否在滚动方向上有约束
           shrinkWrap: false,
+          // 在 CustomScrollView 中，每一个独立的，可滚动的 Widget 被称之为 Sliver
+          // [Flutter-Slivers](https://www.jianshu.com/p/39dab5c40acb)
           slivers: <Widget>[
-            SliverAppBar(
+            // slivers 属性是干嘛的？
+            const SliverAppBar(
               backgroundColor: Colors.transparent,
               // flexibleSpace: HeartImgWidget(Image.asset(Constant.ASSETS_IMG + 'bg_person_center_default.webp')),
+              // flexibleSpace: Image.asset(Constant.ASSETS_IMG + 'bg_person_center_default.webp'),
               expandedHeight: 200.0,
             ),
+
+            // 顶部的提醒视图
             SliverToBoxAdapter(
+              // 横向三个视图：一个小铃铛的图片、一个提醒的文字、一个最右边的向右的箭头
               child: Row(
                 children: <Widget>[
                   Padding(
                     padding: const EdgeInsets.only(left: 10.0, top: 15.0, bottom: 20.0, right: 10.0),
+                    // 一个提醒的图片
                     child: Image.asset(
                       Constant.ASSETS_IMG + 'ic_notify.png',
                       width: 30.0,
                       height: 30.0,
                     ),
                   ),
+                  // 一个提醒的文字
                   const Expanded(
                     child: Text(
                       '提醒',
                       style: TextStyle(fontSize: 17.0),
                     ),
                   ),
+                  // 最右边是一个箭头图标
                   _rightArrow()
                 ],
               ),
             ),
+            // 紧接着下面是一个 暂无新提醒 的文字
             SliverToBoxAdapter(
+              // 高度是 100 的容器 Widget，居中显示
               child: Container(
                 height: 100.0,
                 alignment: Alignment.center,
@@ -63,7 +79,11 @@ class PersonCenterPage extends StatelessWidget {
                 ),
               ),
             ),
+
+            // 一个高度是 10 颜色是灰色的 SliverToBoxAdapter，作为分隔线使用
             _divider(),
+
+            // "我的书影音" 五个大字的 Text Widget
             const SliverToBoxAdapter(
               child: Padding(
                 padding: EdgeInsets.only(left: 10.0, top: 10.0, bottom: 20.0),
@@ -73,19 +93,32 @@ class PersonCenterPage extends StatelessWidget {
                 ),
               ),
             ),
+
+            // 这里是 影音、图书、音乐 三个 Tab
             SliverToBoxAdapter(
               child: Container(
                 child: _VideoBookMusicBookWidget(),
               ),
             ),
+
+            // 一个高度是 10 颜色是灰色的 SliverToBoxAdapter，作为分隔线使用
             _divider(),
+
+            // 书影音数据是否来自网络的开关
             _dataSelect(),
+
+            // 一个高度是 10 颜色是灰色的 SliverToBoxAdapter，作为分隔线使用
             _divider(),
+
+            // 一组左边是一个图标一个文字右边是一个箭头的 SliverToBoxAdapter
             _personItem('ic_me_journal.png', '我的发布'),
             _personItem('ic_me_follows.png', '我的关注'),
             _personItem('ic_me_photo_album.png', '相册'),
             _personItem('ic_me_doulist.png', '豆列 / 收藏'),
+
+            // 一个高度是 10 颜色是灰色的 SliverToBoxAdapter，作为分隔线使用
             _divider(),
+
             _personItem('ic_me_wallet.png', '钱包'),
           ],
         ),
@@ -93,13 +126,15 @@ class PersonCenterPage extends StatelessWidget {
     );
   }
 
+  // 一个向右的箭头的 Icon widget
   _rightArrow() {
-    return Icon(
+    return const Icon(
       Icons.chevron_right,
-      color: const Color.fromARGB(255, 204, 204, 204),
+      color: Color.fromARGB(255, 204, 204, 204),
     );
   }
 
+  // SliverToBoxAdapter 内部是一个高度是 10 颜色是灰色的 Container Widget
   SliverToBoxAdapter _divider() {
     return SliverToBoxAdapter(
       child: Container(
@@ -109,41 +144,50 @@ class PersonCenterPage extends StatelessWidget {
     );
   }
 
-  SliverToBoxAdapter _personItem(String imgAsset, String title, {VoidCallback onTab}) {
+  // 封装一个 _personItem，一个比较常见的样子，左边是一个图标和一个标题文字，右边是一个向右的按钮
+  SliverToBoxAdapter _personItem(String imgAsset, String title, {VoidCallback? onTab}) {
     return SliverToBoxAdapter(
+      // 用 GestureDetector 包裹，指定一个点击事件
       child: GestureDetector(
         behavior: HitTestBehavior.translucent,
+        onTap: onTab,
         child: Row(
           children: <Widget>[
+            // 图标
             Padding(
-              padding: EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
               child: Image.asset(
                 Constant.ASSETS_IMG + imgAsset,
                 width: 25.0,
                 height: 25.0,
               ),
             ),
+
+            // 标题文字
             Expanded(
               child: Text(
                 title,
-                style: TextStyle(fontSize: 15.0),
+                style: const TextStyle(fontSize: 15.0),
               ),
             ),
+
+            // 一个指向右向的按钮
             _rightArrow()
           ],
         ),
-        onTap: onTab,
       ),
     );
   }
 
   _dataSelect() {
-    return UseNetDataWidget();
+    return const UseNetDataWidget();
   }
 }
 
-///这个用来改变书影音数据来自网络还是本地模拟
+// 这个用来改变书影音数据来自网络还是本地模拟
 class UseNetDataWidget extends StatefulWidget {
+  const UseNetDataWidget({super.key});
+
   @override
   _UseNetDataWidgetState createState() => _UseNetDataWidgetState();
 }
@@ -154,69 +198,88 @@ class _UseNetDataWidgetState extends State<UseNetDataWidget> {
   @override
   void initState() {
     super.initState();
+
     _getData();
   }
 
+  // 对应到 iOS 这里是从本地的 user default 中读取数据：一个布尔值判断是从本地读取数据还是从网络加载数据
   _getData() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
+      // 读取数据，默认是 false
       mSelectNetData = prefs.getBool(CacheKey.USE_NET_DATA) ?? false;
     });
   }
 
+  // 往本地写入数据
   _setData(bool value) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    // 写入数据
     prefs.setBool(CacheKey.USE_NET_DATA, value);
   }
 
   @override
   Widget build(BuildContext context) {
+    // SliverToBoxAdapter Widget 放在 CustomScrollView Widget 中
     return SliverToBoxAdapter(
       child: Padding(
+        // 左右距离 10
         padding: const EdgeInsets.only(left: 10.0, right: 10.0),
         child: Row(
           children: <Widget>[
-            Text(
+            // 文字提示
+            const Text(
               '书影音数据是否来自网络',
               style: TextStyle(color: Colors.redAccent, fontSize: 17.0),
             ),
+
+            // 中间隔开
             Expanded(
               child: Container(),
             ),
+
+            // 最右边是一个 iOS 中的 UISwitch 控件，作为开关使用
             CupertinoSwitch(
               value: mSelectNetData,
               onChanged: (bool value) {
                 mSelectNetData = value;
                 _setData(value);
+
+                // ignore: prefer_typing_uninitialized_variables
                 var tmp;
+                // 这里提示用户，设置开关以后需要重启 APP 生效
                 if (value) {
                   tmp = '书影音数据 使用网络数据，重启APP后生效';
                 } else {
                   tmp = '书影音数据 使用本地数据，重启APP后生效';
                 }
+
                 showDialog(
                     context: context,
                     builder: (BuildContext context) {
                       return AlertDialog(
-                        title: Text('提示'),
+                        title: const Text('提示'),
                         content: Text(tmp),
-                        actions: <Widget>[
-                          FlatButton(
-                            child: Text('稍后我自己重启'),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                          FlatButton(
-                            child: Text('现在重启'),
-                            onPressed: () {
-                              RestartWidget.restartApp(context);
-                              Navigator.of(context).pop();
-                            },
-                          )
+                        actions: const <Widget>[
+                          // FlatButton(
+                          //   child: const Text('稍后我自己重启'),
+                          //   onPressed: () {
+                          //     Navigator.of(context).pop();
+                          //   },
+                          // ),
+                          // FlatButton(
+                          //   child: const Text('现在重启'),
+                          //   onPressed: () {
+                          //     RestartWidget.restartApp(context);
+                          //     Navigator.of(context).pop();
+                          //   },
+                          // )
                         ],
                       );
                     });
+
+                // 刷新开关
                 setState(() {});
               },
             )
@@ -227,16 +290,18 @@ class _UseNetDataWidgetState extends State<UseNetDataWidget> {
   }
 }
 
-///影视、图书、音乐 TAB
+// 影视、图书、音乐 TAB
 class _VideoBookMusicBookWidget extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => _VideoBookMusicBookWidgetState();
+  _VideoBookMusicBookWidgetState createState() => _VideoBookMusicBookWidgetState();
 }
 
 class _VideoBookMusicBookWidgetState extends State<_VideoBookMusicBookWidget> with SingleTickerProviderStateMixin {
+  
   @override
   void initState() {
     super.initState();
+
     _tabController = TabController(length: tabTxt.length, vsync: this);
   }
 
@@ -249,8 +314,8 @@ class _VideoBookMusicBookWidgetState extends State<_VideoBookMusicBookWidget> wi
           child: Column(
             children: <Widget>[
               Align(
-                child: _TabBarWidget(),
                 alignment: Alignment.centerLeft,
+                child: _TabBarWidget(),
               ),
               _tabView()
             ],
@@ -261,12 +326,12 @@ class _VideoBookMusicBookWidgetState extends State<_VideoBookMusicBookWidget> wi
   Widget _tabView() {
     return Expanded(
       child: TabBarView(
+        controller: _tabController,
         children: [
           _tabBarItem('bg_videos_stack_default.png'),
           _tabBarItem('bg_books_stack_default.png'),
           _tabBarItem('bg_music_stack_default.png'),
         ],
-        controller: _tabController,
       ),
     );
   }
@@ -276,7 +341,7 @@ class _VideoBookMusicBookWidgetState extends State<_VideoBookMusicBookWidget> wi
       children: <Widget>[
         Expanded(
           child: Padding(
-            padding: EdgeInsets.only(top: 15.0, bottom: 7.0),
+            padding: const EdgeInsets.only(top: 15.0, bottom: 7.0),
             child: Image.asset(
               Constant.ASSETS_IMG + img,
               fit: BoxFit.contain,
@@ -300,31 +365,30 @@ class _VideoBookMusicBookWidgetState extends State<_VideoBookMusicBookWidget> wi
   }
 }
 
-///
-///
 class _TabBarWidget extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _TabBarWidgetState();
 }
 
-TabController _tabController;
+late TabController _tabController;
 
 class _TabBarWidgetState extends State<_TabBarWidget> {
-  Color selectColor, unselectedColor;
-  TextStyle selectStyle, unselectedStyle;
-  List<Widget> tabWidgets;
+  late Color selectColor, unselectedColor;
+  late TextStyle selectStyle, unselectedStyle;
+  late List<Widget> tabWidgets;
 
   @override
   void initState() {
     super.initState();
+
     selectColor = Colors.black;
-    unselectedColor = Color.fromARGB(255, 117, 117, 117);
+    unselectedColor = const Color.fromARGB(255, 117, 117, 117);
     selectStyle = TextStyle(fontSize: 18, color: selectColor);
     unselectedStyle = TextStyle(fontSize: 18, color: selectColor);
     tabWidgets = tabTxt
         .map((item) => Text(
               item,
-              style: TextStyle(fontSize: 15),
+              style: const TextStyle(fontSize: 15),
             ))
         .toList();
   }
@@ -332,9 +396,7 @@ class _TabBarWidgetState extends State<_TabBarWidget> {
   @override
   void dispose() {
     super.dispose();
-    if (_tabController != null) {
-      _tabController.dispose();
-    }
+    _tabController.dispose();
   }
 
   @override
